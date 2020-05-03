1a446e06da823bff370f719d32ed7723397a9643
P-Suttipong <s5935512077@phuket.psu.ac.th>
2020-05-04 06:58:27 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var dgram = require("dgram");
+var server = dgram.createSocket("udp4");
+var track_address;
+var track_port = 1;
+var ack = new Buffer("");
+var app_ack = new Buffer("");
+
+server.on("error", function(err) {
+	console.log("server error:\n"+err.stack);
+	server.close();
+});
+server.on("message", function(msg,rinfo){
+	console.log("server got: "+msg+" from "+rinfo.address+" : "+rinfo.port);
+	if(msg == "connectFromApp"){
+		ack = new Buffer("1");
+	}
+	else if(msg == "tryToConnected"){
+		track_address =  rinfo.address;
+		track_port = rinfo.port;
+		ack = new Buffer("connected");
+		
+	}
+	else if(msg == "forward"){
+		ack = new Buffer("forward");
+	}
+	else if(msg == "backward"){
+		ack = new Buffer("backward");
+	}	
+	server.send(ack,0,ack.length,rinfo.port,rinfo.address, function(err,bytes){
+		console.log("Sended   : "+ack);
+	});
+});
+server.on("listening", function(){
+	var address = server.address();
+	console.log("server listening  "+address.address+":"+address.port);
+});
+server.bind({
+	address: '203.150.107.176',
+	port: 17711,
+	exclusive: true
+});
